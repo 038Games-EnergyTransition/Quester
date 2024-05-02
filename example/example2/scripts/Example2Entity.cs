ab1fb08c23cf70762ed6d3ccfe2ab58c2d45fb4e
Sven Arends <github@svenar.nl>
2024-05-03 00:44:28 +0200

    Added example 2 - Two entity speech interaction

---
+++ example/example2/scripts/Example2Entity.cs
@@ -0,0 +1,92 @@
+using Godot;
+using System;
+
+public partial class Example2Entity : StaticBody2D
+{
+
+	[Export]
+	public QuestResource Quest;
+	[Export]
+    public DataManager _dataManager;
+	[Export]
+	public string BaseKey;
+
+	[Export]
+	public Area2D QuestTrigger;
+	[Export]
+	public Label QuestLabel;
+
+	private QuestResource _quest;
+
+	private QuestManager _questManager = QuestManager.GetInstance();
+
+	public override void _Ready()
+	{
+		_quest = Quest.Instantiate();
+		QuestLabel.Text = "";
+
+		QuestTrigger.BodyEntered += (body) =>
+		{
+			if (body is Example2Player player)
+			{
+				GD.Print("Player entered trigger");
+				_questManager.ResetQuest(_quest);
+				_questManager.StartQuest(_quest);
+			}
+		};
+
+		QuestTrigger.BodyExited += (body) =>
+		{
+			if (body is Example2Player player)
+			{
+				GD.Print("Player exited trigger");
+				_questManager.ResetQuest(_quest);
+			}
+		};
+
+		_questManager.QuestStarted += (QuestResource quest) =>
+		{
+			if (quest == _quest)
+			{
+				GD.Print($"Quest '{_quest.Name}' started");
+			}
+		};
+
+		_questManager.QuestObjectiveAdded += (QuestResource quest, QuestObjective objective) =>
+		{
+			if (quest == _quest)
+			{
+				GD.Print($"Objective '{objective.Description}' added to quest {_quest.Name}");
+			}
+		};
+
+		_questManager.QuestObjectiveCompleted += (QuestResource quest, QuestObjective objective) =>
+		{
+			if (quest == _quest)
+			{
+				GD.Print($"Objective '{objective.Description}' completed in quest {_quest.Name}");
+			}
+		};
+
+		_questManager.QuestCompleted += (QuestResource quest) =>
+		{
+			if (quest == _quest)
+			{
+				_questManager.ResetQuest(_quest);
+				GD.Print($"Quest '{_quest.Name}' completed");
+			}
+		};
+
+		_dataManager.DataChanged += (string key, Variant value) =>
+		{
+			if (key == BaseKey + "_speak")
+			{
+				QuestLabel.Text = (string)value;
+			}
+		};
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ example/example2/scripts/Example2Player.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Example2Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
